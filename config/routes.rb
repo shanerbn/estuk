408176688ac304df8cb8017c446e56c27ea5ab2a
shanerbn <shane@converted.co.uk>
2015-01-24 22:34:29 +0000

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :models
+  devise_for :users
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
